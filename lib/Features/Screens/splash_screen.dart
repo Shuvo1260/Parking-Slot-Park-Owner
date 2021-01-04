cc54999b28a6c4097c75734e3f2de6e8dc0a20dc
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-04 22:32:24 +0600

    Splash screen design is started. Scaffold and Safe area is implemented.

--- lib/Features/Screens/splash_screen.dart
+++ lib/Features/Screens/splash_screen.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+void main() => runApp(
+      MyApp(),
+    );
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(body: SplashScreen()),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    print("Splash");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text("Temp"),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,1 @@
-void main() {
-  // runApp(MyApp());
-}
+
