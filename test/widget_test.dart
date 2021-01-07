ea986cf85fc9f3c8d4dbf2d7e6f60e5163f85aae
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-07 16:03:47 +0600

    Code cleaned.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:parking_slot_seller/main.dart';
+import 'file:///D:/Works/Project/parking_slot_seller/lib/Features/Screens/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
