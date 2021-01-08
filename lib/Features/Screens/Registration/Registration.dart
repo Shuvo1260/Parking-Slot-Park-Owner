f5e13cded01069329a071890a435647db1107eef
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-08 20:54:00 +0600

    Registration page is created.

---
+++ lib/Features/Screens/Registration/Registration.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:parking_slot_seller/Features/Widgets/widgets_login_registration.dart';
+import 'package:parking_slot_seller/Resources/colors.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [COLOR_CARIBBEAN_GREEN, COLOR_SHAMROCK],
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: WidgetTopLogo(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                    ),
+                    // Flexible(
+                    //   flex: 3,
+                    //   // child: DesignRegistrationBottom(),
+                    // ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
