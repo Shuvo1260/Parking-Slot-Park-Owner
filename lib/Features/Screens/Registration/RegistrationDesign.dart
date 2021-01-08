149d1df62c8877a48e770c0ef39af647489c13b6
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-08 21:15:08 +0600

    registration design is created.

---
+++ lib/Features/Screens/Registration/RegistrationDesign.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegistrationDesign extends StatefulWidget {
+  @override
+  _RegistrationDesignState createState() => _RegistrationDesignState();
+}
+
+class _RegistrationDesignState extends State<RegistrationDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
