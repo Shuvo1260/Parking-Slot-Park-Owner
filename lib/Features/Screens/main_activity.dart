66f92cf51e51387d2d3ecee6f113a332790acc2e
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-09 23:34:04 +0600

    MainActivity state full widget is created.

---
+++ lib/Features/Screens/main_activity.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainActivity extends StatefulWidget {
+  @override
+  _MainActivityState createState() => _MainActivityState();
+}
+
+class _MainActivityState extends State<MainActivity> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
