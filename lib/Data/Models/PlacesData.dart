f83c26b4c15f5a0be904aa7570926ebf56503f21
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-16 23:10:11 +0600

    Place dataclass is created.

--- lib/Data/Models/PlacesData.dart
+++ lib/Data/Models/PlacesData.dart
@@ -1,4 +1,9 @@
 import 'package:dataclass/dataclass.dart';
+import 'package:flutter/cupertino.dart';
 
 @dataClass
-class Place {}
+class Place {
+  var id;
+  var address;
+  Place({this.id, @required this.address})
+}
