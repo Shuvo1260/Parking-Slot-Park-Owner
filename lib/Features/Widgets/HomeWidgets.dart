c02916ae13ab6249399b4503d5e3aed67481cabe
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-17 23:24:50 +0600

    PlaceListItem view widget is created.

---
+++ lib/Features/Widgets/HomeWidgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewPlaceListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Card(
+        child: Row(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
