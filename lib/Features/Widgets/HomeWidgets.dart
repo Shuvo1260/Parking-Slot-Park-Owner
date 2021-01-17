5c5840b5daff83381c9f659ba41aa0f414a90e1a
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-17 23:26:29 +0600

    Place data and constructor is created in PlaceList item widget.

--- lib/Features/Widgets/HomeWidgets.dart
+++ lib/Features/Widgets/HomeWidgets.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:parking_slot_seller/Data/Models/PlacesData.dart';
 
 class ViewPlaceListItem extends StatelessWidget {
+  PlaceData placeData;
+
+  ViewPlaceListItem(this.placeData)
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Card(
         child: Row(
-          children: [],
+          children: [
+            
+          ],
         ),
       ),
     );
