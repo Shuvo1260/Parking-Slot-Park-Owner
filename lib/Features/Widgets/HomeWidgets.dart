ec6451ea0a7e090e62f71b6884936bc4303bcc96
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-18 09:54:12 +0600

    List item text is added.

--- lib/Features/Widgets/HomeWidgets.dart
+++ lib/Features/Widgets/HomeWidgets.dart
@@ -12,9 +12,40 @@ class ViewPlaceListItem extends StatelessWidget {
       padding: EdgeInsets.all(10.0),
       child: Card(
         child: Row(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              placeData.imageUrl,
+              height: 80.0,
+              width: 80.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _PlaceListText(placeData.address, fontSize: 20.0,),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _PlaceListText(placeData.rate, fontSize: 16.0),
+              ],
+            )
+          ],
         ),
       ),
     );
+  }
+}
+
+
+class _PlaceListText extends StatelessWidget {
+  var data;
+  var fontSize;
+  _PlaceListText(this.data, {@required this.fontSize})
+  @override
+  Widget build(BuildContext context) {
+    return Text(data, style: TextStyle(
+      color: Colors.black,
+      fontSize: fontSize,
+    ),);
   }
 }
