f5645bf88b8f52eaac7b0c79d52ba6f1a0c29d46
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-20 21:37:17 +0600

    Parking list item widgets is separated in new stateless class.

--- lib/Features/Widgets/HomeWidgets.dart
+++ lib/Features/Widgets/HomeWidgets.dart
@@ -29,41 +29,69 @@ class ViewPlaceListItem extends StatelessWidget {
         ),
         child: Row(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(RADIUS_LIST_ITEM),
-                bottomLeft: Radius.circular(RADIUS_LIST_ITEM),
-              ),
-              child: Image.network(
-                placeData.imageUrl,
-                height: HEIGHT_PARKING_LIST_ITEM_IMAGE,
-                width: WIDTH_PARKING_LIST_ITEM_IMAGE,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Flexible(
-              child: Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _PlaceListText(
-                      placeData.address,
-                      fontSize: 16.0,
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    _PlaceListText(
-                      SIGN_TAKA + placeData.rate.toString(),
-                      fontSize: 12.0,
-                    ),
-                  ],
-                ),
-              ),
-            )
+            _ListItemImage(placeData: placeData),
+            _ListItemFields(placeData: placeData)
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _ListItemFields extends StatelessWidget {
+  const _ListItemFields({
+    Key key,
+    @required this.placeData,
+  }) : super(key: key);
+
+  final PlaceData placeData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _PlaceListText(
+              placeData.address,
+              fontSize: 16.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            _PlaceListText(
+              SIGN_TAKA + placeData.rate.toString(),
+              fontSize: 12.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ListItemImage extends StatelessWidget {
+  const _ListItemImage({
+    Key key,
+    @required this.placeData,
+  }) : super(key: key);
+
+  final PlaceData placeData;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(RADIUS_LIST_ITEM),
+        bottomLeft: Radius.circular(RADIUS_LIST_ITEM),
+      ),
+      child: Image.network(
+        placeData.imageUrl,
+        height: HEIGHT_PARKING_LIST_ITEM_IMAGE,
+        width: WIDTH_PARKING_LIST_ITEM_IMAGE,
+        fit: BoxFit.cover,
       ),
     );
   }
