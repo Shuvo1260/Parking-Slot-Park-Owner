9870d78ab2b1baf01995614b47f1ec7ecc512579
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-20 21:54:57 +0600

    ViewPlace is added.

---
+++ lib/Features/Screens/Home/ViewPlace.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViewPlace extends StatefulWidget {
+  @override
+  _ViewPlaceState createState() => _ViewPlaceState();
+}
+
+class _ViewPlaceState extends State<ViewPlace> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
