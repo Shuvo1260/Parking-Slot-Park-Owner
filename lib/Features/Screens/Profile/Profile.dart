f815d056a100a588f09a1f97eaf1d5af859e5997
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-26 21:43:30 +0600

    Profile picture is added.

--- lib/Features/Screens/Profile/Profile.dart
+++ lib/Features/Screens/Profile/Profile.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:parking_slot_seller/Resources/values.dart';
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Profile"),
-      ),
-    );
+        child: Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(RADIUS_LIST_ITEM),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 3,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 80.0,
+                width: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://img.freepik.com/free-photo/handsome-young-businessman-shirt-eyeglasses_85574-6228.jpg?size=626&ext=jpg",
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
