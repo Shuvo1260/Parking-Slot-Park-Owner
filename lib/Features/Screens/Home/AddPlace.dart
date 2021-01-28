47af618977eb2704f6182e0c93ca2eabebfa274c
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-28 23:27:53 +0600

    Address field is added.

--- lib/Features/Screens/Home/AddPlace.dart
+++ lib/Features/Screens/Home/AddPlace.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:parking_slot_seller/Features/Widgets/AddPlaceWidgets.dart';
 
-class AddPlace extends StatelessWidget {
+class AddPlace extends StatefulWidget {
+  @override
+  _AddPlaceState createState() => _AddPlaceState();
+}
+
+class _AddPlaceState extends State<AddPlace> {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        body: Container(
-          child: Row(
-            children: [],
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                AddPlaceTextField(
+                  (value) {},
+                  obscure: false,
+                  hint: "Address",
+                  keyboardType: TextInputType.streetAddress,
+                ),
+              ],
+            ),
           ),
         ),
       ),
