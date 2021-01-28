48aa1e949de674710e39da24b399b148f1cfbf2e
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-28 23:09:59 +0600

    AddPlaceWidget file is created.

--- lib/Features/Screens/Home/AddPlace.dart
+++ lib/Features/Screens/Home/AddPlace.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class AddPlace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Scaffold(
+        body: Container(
+          child: Row(
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
