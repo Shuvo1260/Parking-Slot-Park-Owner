bfa4b144b2cc5c6cd88e9ca5117b88911d892f46
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-02-04 23:24:35 +0600

    Sign up is implemented.

--- lib/Utils/AuthManager.dart
+++ lib/Utils/AuthManager.dart
@@ -4,45 +4,44 @@ import 'package:parking_slot_seller/Data/Models/UserData.dart';
 import 'package:parking_slot_seller/Resources/strings.dart';
 
 class AuthManager {
-  Firestore _firestore;
+  FirebaseFirestore _firestore;
   FirebaseAuth _firebaseAuth;
   AuthManager() {
-    _firestore = Firestore.instance;
+    _firestore = FirebaseFirestore.instance;
     _firebaseAuth = FirebaseAuth.instance;
   }
 
   Future<bool> isUserExist(String id) async {
-    var value = await _firestore.collection(PATH_USER_DATA).document(id).get();
-    print(value.data);
-    if (value.data != null) return true;
-    return false;
+    var value = await _firestore.collection(PATH_USER_DATA).doc(id).get();
+    print("IsExist: ${value.exists}");
+    return value.exists;
   }
 
   Future<bool> signUP(UserData userData) async {
-    if (await _saveUserData(userData)) {
-      _firebaseAuth
-          .createUserWithEmailAndPassword(
-              email: userData.email, password: userData.password)
-          .then((value) {
-        if (value.user != null) {
-          return true;
-        }
-      });
+    try {
+      var value = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: userData.email.trim(), password: userData.password);
+      if (value.user != null) {
+        await value.user.sendEmailVerification();
+        return await _saveUserData(userData);
+      }
+    } catch (error) {
+      print("SignUpError: $error");
     }
     return false;
   }
 
   Future<bool> _saveUserData(UserData userData) async {
-    _firestore
-        .collection(PATH_USER_DATA)
-        .document(userData.id)
-        .setData(userData.toJSON())
-        .then((value) {
+    try {
+      await _firestore
+          .collection(PATH_USER_DATA)
+          .doc(userData.id)
+          .set(userData.toJSON());
       return true;
-    }, onError: (error) {
+    } catch (error) {
       print("UserDataSavingError: $error");
       return false;
-    });
+    }
   }
 
   Future<void> signIn(email, password) async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'Features/Screens/splash_screen.dart';
 
-void main() => runApp(
-      MyApp(),
-    );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MyApp(),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
