4c3cbe49a5448acfe2c3379c0905d75003781d52
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-02-05 23:25:33 +0600

    Place save data is implemented.

--- lib/Data/Sources/Remote/PlaceDataManager.dart
+++ lib/Data/Sources/Remote/PlaceDataManager.dart
@@ -1,8 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:parking_slot_seller/Data/Models/PlacesData.dart';
+import 'package:parking_slot_seller/Resources/strings.dart';
 
 class PlaceDataManager {
   static Future<bool> saveData(PlaceData placeData) async {
-    return false;
+    try {
+      await FirebaseFirestore.instance
+          .collection(PATH_PLACE_DATA)
+          .doc()
+          .set(placeData.toJSON());
+      return true;
+    } catch (error) {
+      print("PlaceDataSavingError: $error");
+      return false;
+    }
   }
 
   static Future<PlaceData> getData() async {
