8c67f25fb3181295c4c7663667f8585a3bcf9b55
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-02-05 23:22:43 +0600

    PlaceDataManager is created with two method saveData and getData.

---
+++ lib/Data/Sources/Remote/PlaceDataManager.dart
@@ -0,0 +1,11 @@
+import 'package:parking_slot_seller/Data/Models/PlacesData.dart';
+
+class PlaceDataManager {
+  static Future<bool> saveData(PlaceData placeData) async {
+    return false;
+  }
+
+  static Future<PlaceData> getData() async {
+    //
+  }
+}
