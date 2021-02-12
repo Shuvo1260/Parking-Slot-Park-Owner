38f3758a7d82852fe5e6212e0b27c7c8b3a690d8
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-02-13 01:40:04 +0600

    History list is implemented.

---
+++ lib/Controllers/HistoryListController.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:parking_slot_seller/Data/Models/ParkingData.dart';
+import 'package:parking_slot_seller/Resources/strings.dart';
+
+class HistoryListController extends GetxController {
+  FirebaseAuth _firebaseAuth;
+  var parkingList = List<ParkingData>().obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _firebaseAuth = FirebaseAuth.instance;
+    fetchList();
+  }
+
+  void fetchList() async {
+    FirebaseFirestore.instance
+        .collection(PATH_PARKING_DATA)
+        .where('parkOwner', isEqualTo: _firebaseAuth.currentUser.email.trim())
+        .where('status', isEqualTo: 3)
+        .snapshots(includeMetadataChanges: true)
+        .listen((event) {
+      event.docs.forEach((element) {
+        print("Parking: ${element.data()}");
+        var parking = ParkingData();
+        parking.fromJSON(element.data());
+        parkingList.add(parking);
+      });
+    });
+  }
+}
